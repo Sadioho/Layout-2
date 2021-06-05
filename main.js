00c7b73d1bd25a6c7b2e815d9aa45955712cc845
Sadioho <hoxuananh9.3@gmail.com>
2021-06-05 11:40:13 +0700

    custom slick slide

--- main.js
+++ main.js
@@ -40,3 +40,16 @@ $(window).scroll(function() {
 
     }
 }); 
+
+
+$(document).ready(function () {
+    $('.your-class').slick({
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: true,
+      navigation:true,
+
+    });
+  });
